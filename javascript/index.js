c6262461ff33c29c74c45b287d3f955185adfc7a
meysam <meisam.design.ph@gmail.com>
2021-11-21 21:47:19 +0330

    add phone number

--- javascript/index.js
+++ javascript/index.js
@@ -2,13 +2,32 @@
 const titlePhone = document.querySelector('.title-phone');
 const numberPhone = document.querySelector('.number-phone');
 const btnPhone = document.querySelector('.btn-phone');
+const phoneList = document.querySelector('.list-phones');
+const selectType = document.querySelector('.select-type')
 
 //Event listener
-btnPhone.addEventListener('click', additem);
+btnPhone.addEventListener('click', addItem);
 
 
 //Function
-function additem(event){
+function addItem(event){
   event.preventDefault();
-  console.log(event)
+  const divPhone = document.createElement('div');
+  divPhone.classList.add('contacts');
+
+  const contact = document.createElement('li');
+  contact.classList.add('contact-list');
+  const titleContact = document.createElement('span');
+  const numberContact = document.createElement('span');
+  const typeContact = document.createElement('span');
+
+  titleContact.innerText = titlePhone.value;
+  numberContact.innerText = numberPhone.value;
+  typeContact.innerText = selectType.options[selectType.selectedIndex].innerText;
+  contact.appendChild(titleContact)
+  contact.appendChild(numberContact)
+  contact.appendChild(typeContact)
+  divPhone.appendChild(contact)
+  phoneList.appendChild(divPhone);
+  
 }
